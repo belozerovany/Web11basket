4467005da6d487bc16bb88f31911c2ec76e3c740
Nadia <belozerovany@gmail.com>
2017-07-22 11:37:19 +0300

    this is simple adaptive the page basket where you may delete your order and total price will be changed

--- app/js/file-js.js
+++ app/js/file-js.js
@@ -1,15 +1,14 @@
-window.onload = function name() {
-	var mobileMenu  = document.querySelector('.mobile-menu'),
-	    menuUl      = document.querySelector('.menu-ul'),
-	    order       = document.querySelector('.order'),
-	    product     = document.querySelector('.product'),
-	    btnInfo     = document.querySelector('#button-total-info');
-	console.log(order);
-	console.log(product );
-	console.log(menuUl );
-	console.log('btnInfo',btnInfo);
-	
+function handler() {
+	var mobileMenu   = document.querySelector('.mobile-menu'),
+	    menuUl       = document.querySelector('.menu-ul'),
+	    order        = document.querySelector('.block-order'),
+	    myorder      = document.querySelectorAll('.block-order'),
+	    blockProdact = document.querySelectorAll('.main-order'),
+	    container    = document.querySelector('#container-order'),
+	    btnInfo      = document.querySelector('#button-total-info'),
+	    cross 		 = document.querySelectorAll('.cross');
 
+	
 	function openMenu(){
 		console.log('click');
 		if(menuUl.style.display == 'none'){
@@ -19,23 +18,59 @@ window.onload = function name() {
 			console.log('hear');
 		}
 	};
-	function openProduct(){
-		if(product.style.display == 'none'){
-			product.style.display = 'block';
-		} else{
-			product.style.display = 'none';
-			console.log('product');
-		}
-	};
-	function closeBlock(){
-		console.log('ddd');
-		var blockInfo  = document.querySelector('.total-info'),
-			blockOrder = document.querySelector('.decor-order');
-		blockInfo.style.display  ='none';
-		blockOrder.style.display ='block';
-		console.log('block',blockInfo);
+
+
+	function openProductBlock(e){
+		var target = e && e.target || window.e.srcElement;
+		
+		
+		if(target.className == 'order' ){
+			var p 			= target;
+			var block 		= p.parentNode.closest('.order-ul');
+			var divProduct	= block.nextElementSibling;
+			var cards 		= divProduct.querySelector('.cards');
+			var btn 		= divProduct.querySelector('#button-total-info');
+					
+			divProduct.classList.toggle('del');
+
+			cards.addEventListener('click',delblock);
+			btn.addEventListener('click',closeBlock);
+
+			function delblock(e){
+				var nextTarget = e && e.target || window.e.srcElement;
+				
+				if(nextTarget.classList.contains('cross')){
+					var cross = nextTarget;
+	 				var par   = cross.closest('.product-card');
+
+					par.style.display   ='none';
+					var priseString	 	= divProduct.querySelector('.total-price').innerHTML.split(' ');
+
+					var allPriseString  = priseString.join('');
+
+					var priseTotal 		= par.querySelector('.price').innerHTML.split(' ').slice(0,-1);
+
+					var allpriseTotal   = priseTotal.join('');
+
+					
+					divProduct.querySelector('.total-price').innerHTML = parseInt(allPriseString) - parseInt(allpriseTotal);
+					block.querySelector('.price').innerHTML            = parseInt(allPriseString) -  parseInt(allpriseTotal);
+				}
+			}
+			
+			function closeBlock(){
+				var blockInfo  = divProduct.querySelector('.total-info'),
+					blockOrder = divProduct.querySelector('.decor-order');
+
+					blockInfo.style.display  ='none';
+					blockOrder.style.display ='block';
+			}
+
+		}	
 	}
+
 	mobileMenu.addEventListener('click',openMenu);
-	order.addEventListener('click',openProduct);
-	btnInfo.addEventListener('click',closeBlock);
+	container.addEventListener('click',openProductBlock);
+	
 }
+document.addEventListener('DOMContentLoaded',handler);
